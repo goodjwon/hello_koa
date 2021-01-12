736b3684ad9365a9aae373bb393d7111689ed80a
goodjwon <goodjwon@gmail.com>
2021-01-12 15:49:14 +0900

    멀티 라우트

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-const Kor = require('koa')
-const app = new Kor();
+const Koa = require('koa');
+const Router = require('koa-router');
 
-app.use(async (ctx, next) => {
-    console.log(ctx)
-    console.log(1)
-    const started = new Date()
-    await next()
-    console.log(new Date() - started + ' ms')
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next)=>{
+    ctx.body = 'home';
 });
+
+router.get('/about', (ctx, next)=>{
+    ctx.body = 'about';
+});
+
+
+router.get('/about/:name', (ctx, next) =>{
+    const {name} = ctx.params;
+    ctx.body = name + ' is me'
+})
+
+router.get('/post', (ctx, next)=>{
+     const {id} = ctx.request.query;
+     if(id){
+         ctx.body = 'post #'+id;
+     }else{
+         ctx.body = 'No posts id'
+     }
+})
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(4000, ()=>{
-    console.log('hello koa server is listening to port 4000');
-});
+    console.log('heurm server is listening to port 4000');
+})
