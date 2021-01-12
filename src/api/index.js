c16cdb8a428e6355536533a65d42eac12fa87a17
goodjwon <goodjwon@gmail.com>
2021-01-12 16:54:11 +0900

    books 모듈화

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 const Router = require('koa-router');
 
-const api = new Router();
 
-api.get('/books', (ctx, next)=>{
-    ctx.body = 'GET' + ctx.request.path;
-});
+const api = new Router();
+const books = require('./books');
+
+api.use('/books', books.routes());
 
 module.exports = api;
